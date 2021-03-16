980211d51b41010c5f5219cc4272d9715a6d738e
nikkaushal <nikkaushal@gmail>
2021-03-16 16:48:29 -0400

    terraform-databases

--- mysql.tf
+++ mysql.tf
@@ -24,4 +24,14 @@ resource "aws_rds_cluster" "mysql" {
   backup_retention_period             = 5
   preferred_backup_window             = "07:00-09:00"
   db_cluster_parameter_group_name     = aws_rds_cluster_parameter_group.mysql.name
+
+}
+
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "mysql-${var.ENV}-${count.index}"
+  cluster_identifier = aws_rds_cluster.mysql.id
+  instance_class     = "db.t3.small"
+  engine             = aws_rds_cluster.mysql.engine
+  engine_version     = aws_rds_cluster.mysql.engine_version
 }
